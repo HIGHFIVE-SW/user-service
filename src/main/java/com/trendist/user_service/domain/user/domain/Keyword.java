f1be356ef4693a298db2a716d1255b7ee79f1c59
1winhyun <jimking1@naver.com>
2025-04-10 00:30:53 +0900

    feat: User 엔티티 및 Keyword Enum 생성

---
+++ src/main/java/com/trendist/user_service/domain/user/domain/Keyword.java
@@ -0,0 +1,8 @@
+package com.trendist.user_service.domain.user.domain;
+
+public enum Keyword {//게시글 키워드, 협의에 따른 수정 가능
+	Environment,
+	PeopleAndSociety,
+	Economy,
+	Technology
+}
---
+++ src/main/java/com/trendist/user_service/domain/user/domain/User.java
@@ -0,0 +1,50 @@
+package com.trendist.user_service.domain.user.domain;
+
+import java.util.UUID;
+
+import com.trendist.user_service.global.common.domain.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "users")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "user_id")
+	private UUID id;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "email")
+	private String email;
+
+	/*@Column(name = "nickname")
+	private String nickname;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "keyword")
+	private Keyword keyword;
+
+	@Column(name = "exp")
+	@Builder.Default
+	private Integer exp = 0;
+
+	@Column(name="ranking")
+	private Integer ranking;*/
+}
