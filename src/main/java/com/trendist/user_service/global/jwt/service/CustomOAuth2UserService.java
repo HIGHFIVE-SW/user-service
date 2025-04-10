1e081dac12b242e8dd6dc56e96aa70d0c7ac6d0b
1winhyun <jimking1@naver.com>
2025-04-10 17:30:52 +0900

    feat: CustomOAuth2UserService 구현

---
+++ src/main/java/com/trendist/user_service/global/jwt/service/CustomOAuth2UserService.java
@@ -0,0 +1,53 @@
+package com.trendist.user_service.global.jwt.service;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import com.trendist.user_service.domain.user.domain.User;
+import com.trendist.user_service.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+	private final UserRepository userRepository;
+
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		OAuth2UserService<OAuth2UserRequest,OAuth2User> oauth=new DefaultOAuth2UserService();
+		OAuth2User oAuth2User=oauth.loadUser(userRequest);
+
+		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+		String userNameAttributeName=userRequest.getClientRegistration()
+			.getProviderDetails()
+			.getUserInfoEndpoint()
+			.getUserNameAttributeName();
+
+		Map<String,Object> attributes=oAuth2User.getAttributes();
+		String email=(String)attributes.get("email");
+		String username=(String)attributes.get("name");
+
+		User user=userRepository.findByEmail(email)
+			.orElseGet(()->{
+				User newUser=User.builder()
+					.email(email)
+					.username(username)
+					.build();
+				return userRepository.save(newUser);
+			});
+		return new DefaultOAuth2User(
+			Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+			attributes,
+			userNameAttributeName);
+	}
+}
