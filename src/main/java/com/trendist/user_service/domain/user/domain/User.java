583b390a14afa74d8cc07549e2b8e4456acc58df
1winhyun <jimking1@naver.com>
2025-04-12 14:57:55 +0900

    refactor: User 필요없는 import문 삭제

--- src/main/java/com/trendist/user_service/domain/user/domain/User.java
+++ src/main/java/com/trendist/user_service/domain/user/domain/User.java
@@ -6,8 +6,6 @@ import com.trendist.user_service.global.common.domain.BaseTimeEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
