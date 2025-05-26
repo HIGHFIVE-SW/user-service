425ed3b9cbe1707dcb092b394846a5734c6f41d5
swp1017 <swp1017@naver.com>
2025-05-27 02:36:35 +0900

    refactor: 과도한 db 접근 문제 개선

--- src/main/java/com/trendist/user_service/domain/user/service/RankingScheduler.java
+++ src/main/java/com/trendist/user_service/domain/user/service/RankingScheduler.java
@@ -1,6 +1,8 @@
 package com.trendist.user_service.domain.user.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -22,24 +24,33 @@ public class RankingScheduler {
 	@Scheduled(cron = "0 0 0 * * *")
 	@Transactional
 	public void updateUserRanking() {
-		updateAllUserTiers();
-
-		List<Tier> tiers = tierRepository.findAll();
-		for (Tier tier : tiers) {
-			updateUserByTier(tier);
-		}
-	}
-
-	private void updateAllUserTiers() {
 		List<Tier> sortedTiers = tierRepository.findAllByOrderByRequiredExpDesc();
 		List<User> allUsers = userRepository.findAll();
 
+		updateAllUserTiers(allUsers, sortedTiers);
+		updateAllUserRankings(allUsers);
+
+		userRepository.saveAll(allUsers);
+	}
+
+	private void updateAllUserTiers(List<User> allUsers, List<Tier> sortedTiers) {
 		for (User user : allUsers) {
 			Tier newTier = calculateTierByExp(user.getExp(), sortedTiers);
 			user.setTier(newTier);
 		}
+	}
 
-		userRepository.saveAll(allUsers);
+	private void updateAllUserRankings(List<User> allUsers) {
+		Map<String, List<User>> userByTier = allUsers.stream()
+			.collect(Collectors.groupingBy(user -> user.getTier().getTierName()));
+
+		for(Map.Entry<String, List<User>> entry : userByTier.entrySet()) {
+			List<User> users = entry.getValue();
+
+			users.sort((u1, u2) -> Integer.compare(u2.getExp(), u1.getExp()));
+
+			assignRankingByExp(users);
+		}
 	}
 
 	private Tier calculateTierByExp(int exp, List<Tier> sortedTiers) {
@@ -51,9 +62,7 @@ public class RankingScheduler {
 		return sortedTiers.get(sortedTiers.size() - 1);
 	}
 
-	private void updateUserByTier(Tier tier) {
-		List<User> users = userRepository.findByTier_TierNameOrderByExpDesc(tier.getTierName());
-
+	private void assignRankingByExp(List<User> users) {
 		int currentRanking = 1;
 		int position = 1;
 		Integer prevExp = null;
@@ -61,7 +70,7 @@ public class RankingScheduler {
 		for (User user : users) {
 			Integer exp = user.getExp();
 
-			if (exp == 0) {
+			if (exp == 0){
 				user.setRanking(0);
 				position++;
 				continue;
@@ -75,7 +84,5 @@ public class RankingScheduler {
 			prevExp = exp;
 			position++;
 		}
-
-		userRepository.saveAll(users);
 	}
 }
